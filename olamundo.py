f2f2571dccc0123cf56e886965acd6f700ffd257
Cristiano Castro <cscastro87@gmail.com>
2019-06-20 23:45:42 -0300

    init

---
+++ olamundo.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+"""
+# -*- coding:utf-8 -*-
+O comando acima define o encode do script.
+"""
+
+print("Olá mundo")
+print("Outra mensagem")
+# Isto é um comentário
+print("Nova linha")
+"""
+Comentário de linhas multiplas.
+"""
+
+# Operações matemáticas
+print("Operações matemáticas.")
+print("Soma 2 + 2 = ", 2 + 2)
+print(2 + 2)
+print("Subtração 2 - 1 = ", 2 - 1)
+print(2 - 1)
+print("Multiplicação 2 x 3 = ", 2 * 3)
+print(2 * 3)
+print("Divisão 18 / 3 = ", 18 / 3)
+print(18 / 3)
+print("Exponenciação 3^3 = ", 3 ** 3)
+print(3 ** 3)
+
+print("Módulo (resto da divisão) 10 / 3 = ", 10 % 3)
+print(10 % 3)
+
+
---
+++ variaveis.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+minha_variavel = "Olá mundo"
+
+print(minha_variavel)
