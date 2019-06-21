b2d98c18778fb6078abe67d32fb4e2212a386100
Cristiano Castro <cscastro87@gmail.com>
2019-06-21 00:32:29 -0300

    Operadores 2

--- operadores.py
+++ operadores.py
@@ -30,3 +30,47 @@ print("============ Operadores lógicos ============")
 print(x == y and x == soma)
 print(x == y or soma == z)
 print(x == y or soma == z and 2 == soma - y)
+
+# Bloco
+print("============  Blocos (identação)  ============")
+x = 1
+y = 100
+
+if x > y:
+    print("x é maior que y")
+
+if y > x:
+    print("y é maior que x")
+
+if x > y:
+    print("x é maior que y")
+else:
+    print("x não é maior que y")
+
+# Tabulação
+
+print("============  Tribulação digo, tabulação  ============")
+a = 7
+b = 6
+
+if b > a:
+    if b > 0:
+        print("b é marior que a\nb é positivo")
+    else:
+        print("b não é maior nem positivo")
+else:
+    print("b é menor que a")
+
+x = 1
+y = 2
+
+# elif
+print("============  elif  ============")
+if x == y:
+    print("numeros iguais")
+elif y > x:
+    print("y maior que x")
+else: 
+    print("numeros diferentes")
+
+
