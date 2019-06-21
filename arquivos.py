b5293f331ab8d903b1eeda099d5c638d9be17bb7
Cristiano Castro <cscastro87@gmail.com>
2019-06-21 19:01:18 -0300

    funcoes

---
+++ arquivos.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+'''
+Manipulação de arquivos
+
+Modos:
+r       somente leitura
+w       escrita (caso o arquivo já exista, ele será apagado e outro criado em seu lugar)
+a       leitura e escrita (adiciona novo conteúdo no fim do arquivo)
+r+      leitura e escrita
+w+      escrita (w+ assim como w também apagará o conteúdo anterior)
+a+      leitura e escrita (abre o arquivo para atualização)
+'''
+
---
+++ funcoes.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+'''
+Funções em Python são definidas pela palavra def
+'''
+def soma(x, y):
+    valorSomado = x + y 
+    return valorSomado
+def valorMultiplicacao(x, y):
+    return x * y
+
+valorSomado = soma(2,3)
+print(valorSomado)
+
+valorMultiplicacao = valorMultiplicacao(5,3)
+print(valorMultiplicacao)
+
+print(soma(valorSomado, valorMultiplicacao))
