56a9cc57f0f050f6af261b8d7a35a5de18e84835
Cristiano Castro <cscastro87@gmail.com>
2019-06-21 00:13:48 -0300

    Operadores

--- operadores.py
+++ operadores.py
@@ -1,1 +1,32 @@
 # -*- coding:utf-8 -*-
+x = 2
+y = 3
+z = 5
+
+print(x == y)
+print(x < y)
+
+#Operadores relacionais
+soma = x + y
+print(soma)
+print(soma == x)
+print(soma == y)
+print(soma > x)
+print(soma > y)
+print(soma < x)
+print(soma < y)
+print(soma >= y)
+print(soma <= y)
+print(soma >= x)
+print(soma <= x)
+
+"""
+Operadores lógicos
+AND
+OR
+NOT
+"""
+print("============ Operadores lógicos ============")
+print(x == y and x == soma)
+print(x == y or soma == z)
+print(x == y or soma == z and 2 == soma - y)
