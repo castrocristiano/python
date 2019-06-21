53bf57a97b75797754578885c58505ec17ba4970
Cristiano Castro <cscastro87@gmail.com>
2019-06-21 00:01:57 -0300

    Variáveis

--- variaveis.py
+++ variaveis.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
-minha_variavel = "Olá mundo"
+#String
+minha_variavel = "Eu sou uma string"
+#Inteira
+varInt = 1 
+#Float
+varFloat = 2.2
+#Booleana
+varBooleana = True
+varBooleana = False
 
 print(minha_variavel)
+print(varInt)
+print(varFloat)
+print(varBooleana)
