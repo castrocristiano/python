3ca9be7658bf5f37dd69a27900e7e9ebdc27b002
Cristiano Castro <cscastro87@gmail.com>
2019-06-22 10:33:23 -0300

    Arquivos

--- arquivos.py
+++ arquivos.py
@@ -11,3 +11,28 @@ w+      escrita (w+ assim como w também apagará o conteúdo anterior)
 a+      leitura e escrita (abre o arquivo para atualização)
 '''
 
+pasta = "./arquivos/"
+arquivo = open(pasta + "arquivo.txt")
+linhas = arquivo.readlines()
+
+for linha in linhas:
+    print(linha)
+
+arquivo.close()
+
+# criando um arquivo
+arquivo2 = open(pasta + "arquivo2.txt", "w")
+arquivo2.encoding
+
+arquivo2.write("Esse é o meu arquivo numero 2\n")
+
+arquivo2.close()
+
+arquivo2 = open(pasta + "arquivo2.txt", "a")
+
+for linha in linhas:
+    arquivo2.write(linha)
+
+arquivo2.close()
+
+
